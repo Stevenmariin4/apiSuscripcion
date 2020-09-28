cb505b21502580e24ea970e8db93e3ad34e600ff
Steven Marin <jarevalo@robotec.com>
2020-09-28 09:31:51 -0500

    configuracion variables de entorno

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,15 +3,21 @@ import * as sequelize from "sequelize";
 import { Modelrol } from "../models/role.model";
 import { ModelSuscripcion } from "../models/suscripcion.model";
 import { Modeluser } from "../models/user.model";
+require("dotenv").config();
 export const sql = new sequelize.Sequelize(
-  config.DB,
-  config.USER,
-  config.PASSWORD,
+  process.env.DB || "",
+  process.env.USER || "",
+  process.env.PASSWORD || "4MewCAqnytH4QFU7TAlX",
   {
     port: 3306,
-    host: config.HOST,
-    dialect: "mysql",
-    pool: config.pool,
+    host: process.env.HOST || "",
+    dialect:"mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
 );
 
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,20 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { sql } from "./database/database";
 import router from "./routes/routes";
+require('dotenv').config()
 const app = express();
 // Sincronizacion base de datos
-sql.sync({ force: true }).then(() => {
+sql.sync({ force: false }).then(() => {
   console.log("Data Base Sync");
 });
+
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", router);
 
 // Seetings
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
